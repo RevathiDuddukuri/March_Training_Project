07a7622f91f05f52ae8270af7f0616ed2d9ec5b0
redudduk <redudduk@DIN80012030.corp.capgemini.com>
2018-03-30 12:50:55 +0530

    for loop commit

--- Day2-Project/src/main/java/org/cap/demo/SalesClass.java
+++ Day2-Project/src/main/java/org/cap/demo/SalesClass.java
@@ -4,6 +4,10 @@ public class SalesClass {
 	
 	public void show(){
 int[] arg={12,45,67,90};
+
+for (int i = 0; i < arg.length; i++) {
+	System.out.println(arg[i]);
+}
 		
 		System.out.println("Show Method");
 		System.out.println("Show Method for Git");
